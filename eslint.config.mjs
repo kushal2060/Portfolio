9726f764cec13f900180a72b43d4304c256385ba
Kushal-acharya <https://github.com/kushal2060>
2025-05-11 19:32:03 +0545

    change

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,34 @@
-// eslint.config.mjs
-import next from 'eslint-config-next';
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
-export default [
-  next(),
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        project: "./tsconfig.json",
+        tsconfigRootDir: __dirname,
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
+      next: require("eslint-plugin-next"),
+    },
+    rules: {
+      // Optional: your custom rules here
+    },
+  },
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
+
+export default eslintConfig;
