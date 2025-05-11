abfcdd3fec6b4a0da371692c33933fc5504cd66b
Kushal-acharya <https://github.com/kushal2060>
2025-05-11 21:38:21 +0545

    FINAL

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -5,16 +5,16 @@ import { Facebook, Mail, Github, Instagram } from 'lucide-react';
 export default function ContactForm() {
   return (
     <div className="flex justify-center gap-6  text-black dark:text-white ">
-    <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+    <a href="https://www.facebook.com/kushal.acha" target="_blank" rel="noopener noreferrer">
       <Facebook className="w-6 h-6 hover:scale-110 transition-transform" />
     </a>
     <a href="mailto:kushalacharya507@email.com">
       <Mail className="w-6 h-6 hover:scale-110 transition-transform" />
     </a>
-    <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer">
+    <a href="https://github.com/kushal2060" target="_blank" rel="noopener noreferrer">
       <Github className="w-6 h-6 hover:scale-110 transition-transform" />
     </a>
-    <a href="https://instagram.com/yourhandle" target="_blank" rel="noopener noreferrer">
+    <a href="https://instagram.com/kus_sa_l" target="_blank" rel="noopener noreferrer">
       <Instagram className="w-6 h-6 hover:scale-110 transition-transform" />
     </a>
   </div>
